b205cd950ed64cb974e9d2f9a50fd92f92449384
xuyt <472677485@qq.com>
2021-08-06 15:46:42 +0800

    1

---
+++ aProject/src/com/xyt/Test1.java
@@ -0,0 +1,15 @@
+package com.xyt;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("xyt");
+		System.out.println("zhx");
+		System.out.println("xxt");
+		System.out.println("xzh");
+		System.out.println("xbh");
+		System.out.println("czh");
+	}
+
+}
---
+++ aProject/src/com/xyt/TestVersion3.java
@@ -0,0 +1,10 @@
+package com.xyt;
+
+public class TestVersion3 {
+	int j=0;
+	int m=0;
+	
+	public void paint(){
+		System.out.println( j+m);
+	}
+}
